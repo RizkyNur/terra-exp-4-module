5e2bc59ed85cdeed7d468fab09792589a9e73daa
RizkyNur <rizky.nurfaizi@ui.ac.id>
2019-07-11 16:51:35 +0700

    First module terra commit

---
+++ s3/variables.tf
@@ -0,0 +1,11 @@
+variable "s3" {
+  type          = "string"
+  description   = "profile used"
+  default       = "terra-exp-4-s3"
+}
+
+variable "s3-static" {
+  type          = "string"
+  description   = "region name"
+  default       = "terra-exp-4-s3-static"
+}
