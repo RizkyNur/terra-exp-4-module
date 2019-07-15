a84d2903ba5bec5d0a04b817063de4d60cd65fb4
RizkyNur <rizky.nurfaizi@ui.ac.id>
2019-07-15 14:21:28 +0700

    Second try

--- s3/main.tf
+++ s3/main.tf
@@ -3,13 +3,13 @@ resource "aws_s3_bucket" "s3" {
   acl       = "public-read"
 }
 
-resource "aws_s3_bucket" "s3-sandbox-static" {
-  bucket    = "${var.s3-static}"
-  acl       = "public-read"
-  cors_rule {
-    allowed_headers = ["Authorization"]
-    allowed_methods = ["GET"]
-    allowed_origins = ["*"]
-    max_age_seconds = 3000
-  }
-}
+# resource "aws_s3_bucket" "s3-sandbox-static" {
+#   bucket    = "${var.s3-static}"
+#   acl       = "public-read"
+#   cors_rule {
+#     allowed_headers = ["Authorization"]
+#     allowed_methods = ["GET"]
+#     allowed_origins = ["*"]
+#     max_age_seconds = 3000
+#   }
+# }
